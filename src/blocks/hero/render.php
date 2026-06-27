757d241ef0942721c3002f210ba90fcf79499807
john-mcpherson <mcpherso@proton.me>
2026-06-27 16:11:53 +0100

    convert theme to jm theme

--- src/blocks/hero/render.php
+++ src/blocks/hero/render.php
@@ -5,7 +5,7 @@ $background_image = "https://placehold.net/800x600.png";
 $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt tempore, sint nulla eaque similique a ex assumenda minima dicta illo totam ullam vitae iure cupiditate id officiis accusantium rerum ipsum.";
 
 ?>
-<section class="amc-section amc-hero">
+<section class="jm-section jm-hero">
     <div class="hero-text">
         <h1><?php echo esc_html($heading) ?></h1>
         <p>
